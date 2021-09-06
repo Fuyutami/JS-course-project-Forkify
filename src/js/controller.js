d76fca9f8d339204115c40cc427437b8b08b2b85
[Fuyutami] <fuyutami.art@gmail.com>
2021-09-06 15:44:43 +0300

    first commit

---
+++ src/js/controller.js
@@ -0,0 +1,39 @@
+import * as model from './model.js'
+import recipeView from './views/recipeView'
+
+
+import 'core-js/stable' // for polyfilling everything else
+import 'regenerator-runtime/runtime' // for polyfilling async-await
+
+
+const recipeContainer = document.querySelector('.recipe');
+
+
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1)
+    console.log(id)
+
+    if(!id) return
+    
+    recipeView.renderSpinner()
+   
+    // Loading Recipe
+    await model.loadRecipe(id)
+
+    // Rendering Recipe
+    recipeView.render(model.state.recipe)
+    
+  } catch (err) {
+    alert(err)
+  }
+};
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes))
